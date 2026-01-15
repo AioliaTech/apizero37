e2df6caf329cf48bc29bbd0aafc87477d6613866
AioliaTech <aioliatechrs@gmail.com>
2026-01-15 15:57:10 -0300

    Update carburgo_parser.py

--- fetchers/carburgo_parser.py
+++ fetchers/carburgo_parser.py
@@ -14,7 +14,7 @@ class CarburgoParser(BaseParser):
         """Verifica se pode processar dados do Carburgo"""
         if not url:
             return False
-        return "citroenpremiere.com.br" in url.lower()
+        return "kiaautovale.com.br" in url.lower()
     
     def parse(self, data: Any, url: str) -> List[Dict]:
         """Processa dados do Carburgo"""
